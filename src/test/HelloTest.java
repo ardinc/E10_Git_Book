39943fce9c95199f7b26faf5490cfa67385911c5
ardinc <ardin.ac@gmail.com>
2022-12-12 22:09:12 +0100

    Created page 3

---
+++ src/test/HelloTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class HelloTest {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Developing hello feature");
+    }
+}
